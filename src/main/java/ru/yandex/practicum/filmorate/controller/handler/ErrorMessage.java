a6a3efb5a4383c8e1f04eb5e8fd93444cdea89fe
Alexander Matushin <fixercom@yandex.ru>
2022-11-11 09:51:45 +0300

    feat: добавить обработчик ошибок

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/handler/ErrorMessage.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.controller.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private int statusCode;
+    private String message;
+}
