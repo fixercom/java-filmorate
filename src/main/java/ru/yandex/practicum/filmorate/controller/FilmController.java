c92988c478f6324f4e0fe5f9123a04f8b400380c
Alexander Matushin <fixercom@yandex.ru>
2022-11-23 14:29:19 +0300

    feat: добавить новые эндпоинты в контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,36 +1,63 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/films")
+@RequiredArgsConstructor
 @Slf4j
-@AllArgsConstructor
 public class FilmController {
     private final FilmStorage filmStorage;
+    private final FilmService filmService;
 
     @GetMapping
     public List<Film> getAllFilms() {
-        log.debug("Получен GET запрос /films");
+        log.debug("Сервер получил GET запрос '/films'");
         return filmStorage.getAllFilms();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Film getFilmById(@PathVariable Long id) {
+        log.debug("Сервер получил GET запрос '/films/{}'", id);
+        return filmStorage.getFilmById(id);
     }
 
     @PostMapping
     public Film createFilm(@RequestBody @Valid Film film) {
-        log.debug("Получен POST запрос /films \n\trequest body: {}", film);
+        log.debug("Сервер получил POST запрос '/films' request body '{}'", film);
         return filmStorage.createFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody @Valid Film film) {
-        log.debug("Получен PUT запрос /films \n\trequest body: {}", film);
+        log.debug("Сервер получил PUT запрос '/films' request body '{}'", film);
         return filmStorage.updateFilm(film);
+    }
+
+    @PutMapping(value = "/{id}/like/{userId}")
+    public void addLikeToFilm(@PathVariable Long id, @PathVariable Long userId) {
+        log.debug("Сервер получил PUT запрос '/films/{}/like/{}'", id, userId);
+        filmService.addLikeToFilm(id, userId);
+    }
+
+    @DeleteMapping(value = "/{id}/like/{userId}")
+    public void removeLikeFromFilm(@PathVariable Long id, @PathVariable Long userId) {
+        log.debug("Сервер получил DELETE запрос '/films/{}/like/{}'", id, userId);
+        filmService.removeLikeFromFilm(id, userId);
+    }
+
+    @GetMapping(value = "/popular")
+    public List<Film> getTopFilms(@RequestParam Optional<Integer> count) {
+        log.debug("Сервер получил GET запрос '/popular'");
+        return filmService.getTopFilms(count.orElse(10));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,36 +1,68 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
+@RequiredArgsConstructor
 @Slf4j
-@AllArgsConstructor
 public class UserController {
     private final UserStorage userStorage;
+    private final UserService userService;
 
     @GetMapping
     public List<User> getAllUsers() {
-        log.debug("Получен GET запрос /users");
+        log.debug("Сервер получил GET запрос '/users'");
         return userStorage.getAllUsers();
+    }
+
+    @GetMapping(value = "/{id}")
+    public User getUserById(@PathVariable Long id) {
+        log.debug("Сервер получил GET запрос '/users/{}'", id);
+        return userStorage.getUserById(id);
     }
 
     @PostMapping
     public User createUser(@RequestBody @Valid User user) {
-        log.debug("Получен POST запрос /users \n\trequest body: {}", user);
+        log.debug("Сервер получил POST запрос '/users' request body '{}'", user);
         return userStorage.createUser(user);
     }
 
     @PutMapping
-    public User updateFilm(@RequestBody @Valid User user) {
-        log.debug("Получен PUT запрос /users \n\trequest body: {}", user);
+    public User updateUser(@RequestBody @Valid User user) {
+        log.debug("Сервер получил PUT запрос '/users' request body '{}'", user);
         return userStorage.updateUser(user);
+    }
+
+    @PutMapping(value = "/{id}/friends/{friendId}")
+    public void addFriendForUser(@PathVariable Long id, @PathVariable Long friendId) {
+        log.debug("Сервер получил PUT запрос '/users/{}/friends/{}'", id, friendId);
+        userService.addFriendForUser(id, friendId);
+    }
+
+    @DeleteMapping(value = "/{id}/friends/{friendId}")
+    public void removeFriendFromUser(@PathVariable Long id, @PathVariable Long friendId) {
+        log.debug("Сервер получил DELETE запрос '/users/{}/friends/{}'", id, friendId);
+        userService.removeFriendFromUser(id, friendId);
+    }
+
+    @GetMapping(value = "/{id}/friends")
+    public List<User> getAllFriends(@PathVariable Long id) {
+        log.debug("Сервер получил GET запрос '/users/{}/friends'", id);
+        return userService.getAllFriends(id);
+    }
+
+    @GetMapping(value = "/{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        log.debug("Сервер получил GET запрос '/users/{}/friends/common/{}'", id, otherId);
+        return userService.getCommonFriends(id, otherId);
     }
 }
